bc5a247e625d41189999b1509a3abc38a728818f
Josh Terrill <joshterrill.dev@gmail.com>
2017-03-26 03:04:15 -0700

    updated index

--- index.js
+++ index.js
@@ -1,27 +1,32 @@
-#!/usr/bin/env node
-var pkg = require('./package.json'),
-    pkgDependencies = Object.getOwnPropertyNames(pkg.dependencies),
-    pkgDevDependencies = Object.getOwnPropertyNames(pkg.devDependencies),
-    fs = require('fs');
-
+var pkg = require('./package.json');
+var fs = require('fs');
+var path = require('path');
 // check if package.json exists
 fs.exists('./package.json', function(dir) {
   if (!dir) throw 'Cannot find ./package.json file';
   // check if node_modules exists
   fs.exists('./node_modules', function(dir) {
     if (!dir) throw 'Cannot find ./node_modules/ directory';
-    // loop over dependencies
-    pkgDependencies.forEach(function(prop) {
-      // find the version and set it in the pkg variable
-      var version = JSON.parse(fs.readFileSync(`./node_modules/${prop}/package.json`).toString()).version;
-      pkg['dependencies'][prop] = version;
-    });
-    // loop over dev dependencies
-    pkgDevDependencies.forEach(function(prop) {
-      // find the version and set it in the pkg variable      
-      var version = JSON.parse(fs.readFileSync(`./node_modules/${prop}/package.json`).toString()).version;
-      pkg['devDependencies'][prop] = version;
-    });
+    if (pkg.dependencies) {
+      var pkgDependencies = Object.getOwnPropertyNames(pkg.dependencies);
+      // loop over dependencies
+      pkgDependencies.forEach(function(prop) {
+        // find the version and set it in the pkg variable
+        var version = JSON.parse(fs.readFileSync(`./node_modules/${prop}/package.json`).toString()).version;
+        pkg['dependencies'][prop] = version;
+      });
+    }
+
+    if (pkg.devDependencies) {
+      var pkgDevDependencies = Object.getOwnPropertyNames(pkg.devDependencies);
+      // loop over dev dependencies
+      pkgDevDependencies.forEach(function(prop) {
+        // find the version and set it in the pkg variable      
+        var version = JSON.parse(fs.readFileSync(`./node_modules/${prop}/package.json`).toString()).version;
+        pkg['devDependencies'][prop] = version;
+      });
+    }
+    
     // write the pkg variable out to package.json
     fs.writeFileSync('./package.json', JSON.stringify(pkg, null, 2) , 'utf-8');
   });
